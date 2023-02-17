f1bf8f181c1da43d2bb285f987c4effa196928b1
HumairaIrin <humairamuntaha24@gmail.com>
2023-02-18 02:09:30 +0600

    freshers job read method added

--- index.js
+++ index.js
@@ -13,12 +13,21 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tgametl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    const freshersJobCollection = client.db('jobifyHMI').collection('freshersJob');
+    try{
+        app.get('/freshersJob', async (req, res)=>{
+            const query = {};
+            const freshersJob = await freshersJobCollection.find(query).toArray();
+            res.send(freshersJob);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 app.get('/',(req, res) => {
